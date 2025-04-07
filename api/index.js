0b49c164f9c3190ab27fe3a8b1156508fc1a3982
ahmadromadhon-code <155276042+ahmadromadhon-code@users.noreply.github.com>
2025-04-07 10:21:55 +0700

    Fix vercel deployment setup

--- api/index.js
+++
@@ -1,5 +0,0 @@
-require('dotenv').config({ path: './.env' });
-const app = require('./app');
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
