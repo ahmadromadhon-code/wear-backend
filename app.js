bb3ab032dd711e26e5f998b5049e2834a71fcf67
ancientmoon <155276042+ahmadromadhon-code@users.noreply.github.com>
2025-04-07 11:24:04 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,42 @@
-require('dotenv').config(); // disarankan tetap load di sini juga
-
+require('dotenv').config();
 const express = require('express');
-const app = express();
-const connectDB = require('./db');
-const productRoutes = require('./routes/productRoutes');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const path = require('path');
+const productRoutes = require('./routes/productRoutes');
 
-// Koneksi ke MongoDB
-connectDB();
+const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
 // Routing API
-app.get('/', (req, res) => {
-  res.json({ message: 'API is running' });
-});
+app.use('/api/products', productRoutes);
 
+// Serve frontend statis
+app.use(express.static(path.join(__dirname, '../frontend')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/index.html'));
+});
 
 // 404 Handler
 app.use((req, res, next) => {
   res.status(404).json({ message: 'Endpoint tidak ditemukan' });
 });
 
-// Error handler global
+// Global Error handler
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: 'Terjadi kesalahan pada server' });
 });
+
+// Koneksi ke MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch((err) => console.log(err));
 
 module.exports = app;
